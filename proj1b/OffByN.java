27dfa7665fcd0017bd38046a27add9b843e0c9dd
shawn1999k <1227681738@qq.com>
2021-08-29 12:00:50 +0800

    considered the non-lower case

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -4,7 +4,7 @@ public class OffByN implements CharacterComparator {
         N = x;
     }
     public boolean equalChars(char x, char y) {
-        if (Math.abs(x - y) == N) {
+        if (Math.abs(x - y) == N && Character.isLowerCase(x) && Character.isLowerCase(y)) {
             return true;
         }
         return false;
