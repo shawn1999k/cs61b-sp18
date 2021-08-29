cee57a81ceef2e4aa61e732924d842ef0e620822
shawn1999k <1227681738@qq.com>
2021-08-29 22:13:40 +0800

    made little improvements

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,8 +1,7 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        if (Math.abs(x - y) == 1 && ((Character.isLowerCase(x) && Character.isLowerCase(y))
-                || (Character.isUpperCase(x) && Character.isUpperCase(y)))) {
+        if (Math.abs(x - y) == 1) {
             return true;
         }
         return false;
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,9 +10,12 @@ public class TestOffByOne {
     @Test
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('r', 'q'));
+        assertFalse(offByOne.equalChars('a', 'c'));
         assertTrue(offByOne.equalChars('A', 'B'));
-        assertFalse(offByOne.equalChars(' ', '!'));
+        assertFalse(offByOne.equalChars('A', 'C'));
+        assertTrue(offByOne.equalChars(' ', '!'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('<', '$'));
         assertFalse(offByOne.equalChars('A', 'b'));
     }
 }
